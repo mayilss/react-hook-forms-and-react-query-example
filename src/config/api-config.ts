3942f4fbf93cb45157d7001a2ff35f672f04d704
mayilss <mayilsafarzada@gmail.com>
2024-02-11 16:40:37 +0400

    libraries added

--- src/config/api-config.ts
+++ src/config/api-config.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const baseApi = axios.create({
-  baseURL: `${import.meta.env.BASE_URL}/todos.json`,
+  baseURL: import.meta.env.VITE_BASE_URL,
 });
 
 export default baseApi;
