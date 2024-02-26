d14f69b053a85b42146e4666c9ade1a3638fe7ea
mayilss <mayilsafarzada@gmail.com>
2024-02-26 21:21:19 +0400

    toastify added

--- src/main.tsx
+++ src/main.tsx
@@ -1,14 +1,17 @@
 import { QueryClientProvider } from "@tanstack/react-query";
 import React from "react";
 import ReactDOM from "react-dom/client";
+import { ToastContainer } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
 import App from "./App.tsx";
-import "./index.css";
 import queryClient from "./config/query-client.ts";
+import "./index.css";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
     <QueryClientProvider client={queryClient}>
       <App />
+      <ToastContainer />
     </QueryClientProvider>
   </React.StrictMode>
 );
